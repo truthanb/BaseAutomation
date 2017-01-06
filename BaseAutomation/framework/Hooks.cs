757185252284ebfeec6e85f8b8c483a0ba871587
Ben <Ben@DESKTOP-QGCGT12>
2017-01-06 00:32:14 -0500

    Add project files.

---
+++ BaseAutomation/framework/Hooks.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using automation_framework.framework;
+
+namespace BaseAutomation.framework
+{
+    [Binding]
+    public class Hooks
+    {
+        [BeforeFeature("setupFeature")]
+        public static void BeforeAll()
+        {
+            Env.LoadConfig();
+            Env.SetBrowser();
+            Env.GetBrowser().Navigate().GoToUrl(Env.GetBaseUrl());
+        }
+
+        [AfterScenario]
+        public static void AfterEach()
+        {
+            try
+            {
+
+                IWebDriver browser = Env.GetBrowser();
+                browser.Manage().Cookies.DeleteAllCookies();
+                browser.FindElement(By.Id("logout")).Click();
+                browser.Manage().Cookies.DeleteAllCookies();
+                BrowserFactory.CloseAllDrivers();
+            }
+            catch (Exception) { }
+        }
+
+        [AfterTestRun]
+        private static void afterAll()
+        {
+            BrowserFactory.CloseAllDrivers();
+        }
+    }
+}
---
+++ BaseAutomation/step_definitions/FrameworkTesting.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+using BaseAutomation.framework;
+using BaseAutomation.page_objects;
+using System.Threading;
+
+
+
+
+namespace BaseAutomation.step_definitions
+{
+    [Binding]
+    public sealed class FrameworkTesting
+    {
+
+        [Given(@"I have gone to heroku app")]
+        public void GivenIHaveGoneToHerokuApp()
+        {
+            HerokuLoginPage page = new HerokuLoginPage();
+            page.userNameInput.SendKeys("tomsmith");
+            page.passwordInput.SendKeys("SuperSecretPassword!");
+            page.submitButton.Click();
+        }
+
+        [Then(@"the result should be 120 on the screen")]
+        public void blahp()
+        {
+            Thread.Sleep(10000);
+        }
+
+    }
+}
