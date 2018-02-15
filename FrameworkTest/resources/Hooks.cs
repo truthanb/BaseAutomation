cdf9b0da09702523c96ca148dca6f1133567747a
Benjamin Truthan <ben.truthan.w@gmail.com>
2018-02-15 09:05:45 -0500

    Update Hooks.cs

--- FrameworkTest/resources/Hooks.cs
+++ FrameworkTest/resources/Hooks.cs
@@ -1,14 +1,15 @@
-﻿using System;
+using System;
 using TechTalk.SpecFlow;
 using OpenQA.Selenium;
 using AutomationFramework.framework;
 using RelevantCodes.ExtentReports;
 
-namespace BaseAutomation.framework
+namespace FrameworkTest.resources
 {
     [Binding]
     public class Hooks
     {
+        private static ExtentTest test;
         //More information about how hooks work can be found here. http://specflow.org/documentation/Hooks/
 
         [BeforeTestRun()]
@@ -16,46 +17,65 @@ namespace BaseAutomation.framework
         {
             Env.LoadConfig();
             Env.SetBrowser();
-            Env.GetBrowser().Navigate().GoToUrl(Env.GetBaseUrl());
             Env.InitializeExtentReport();
+            Env.GetBrowser().Navigate().GoToUrl(Env.GetBaseUrl());
+            Env.GetExtentReport().AddSystemInfo("Browser", Env.config.browser);
+        }
+
+        [BeforeFeature]
+        public static void BeforeFeature()
+        {
+
         }
 
         [BeforeScenario]
-        public static void BeforeScenario()
+        public void BeforeScenario()
         {
-            Env.BeginExtentTestLogging(ScenarioContext.Current.ScenarioInfo.Title, "sample");
+            //Get the scenario title to pass in for the test logging
+            string testName = ScenarioContext.Current.ScenarioInfo.Title;
+
+            //Start logging for the scenario using the scenario title
+            test = Env.GetExtentReport().StartTest(testName);
         }
 
-        [BeforeStep()]
-        public static void BeforeStep()
+        [BeforeStep]
+        public void BeforeStep()
         {
-
         }
 
-        [AfterStep()]
-        public static void AfterStep()
+        [AfterStep]
+        public void AfterStep()
         {
+            var stepName = ScenarioStepContext.Current.StepInfo.StepDefinitionType.ToString() + " " + ScenarioStepContext.Current.StepInfo.Text;
+            var error = ScenarioContext.Current.TestError;
+
             if (ScenarioContext.Current.TestError != null)
             {
-                var error = ScenarioContext.Current.TestError;
-                Env.test.Log(LogStatus.Error, ScenarioContext.Current.StepContext.StepInfo.StepDefinitionType +" "+
-                    ScenarioContext.Current.StepContext.StepInfo.Text ,error.Message + error.Source + error.StackTrace);
+                test.Log(LogStatus.Info, stepName, error);
             }
             else
             {
-                string message = "Test Step: " + ScenarioContext.Current.StepContext.StepInfo.Text + "PASSED";
-                Env.test.Log(LogStatus.Pass, message);
+                test.Log(LogStatus.Info, stepName);
             }
         }
 
         [AfterScenario]
         public static void AfterEach()
         {
-            Env.EndExtentTest();
-            RecordTestResult();
+            if (ScenarioContext.Current.TestError != null)
+            {
+                var error = ScenarioContext.Current.TestError;
+                test.Log(LogStatus.Fail, error);
+            }
+            else
+            {
+                var scenarioName = ScenarioContext.Current.ScenarioInfo.Title;
+                test.Log(LogStatus.Pass, scenarioName);
+            }
 
             try
             {
+
                 IWebDriver browser = Env.GetBrowser();
                 browser.Manage().Cookies.DeleteAllCookies();
                 browser.FindElement(By.Id("logout")).Click();
@@ -64,27 +84,18 @@ namespace BaseAutomation.framework
             catch (Exception) { }
         }
 
-        [AfterTestRun]
-        private static void afterAll()
+        [AfterFeature]
+        public static void AfterFeature()
         {
-            Env.ClearConfig();
-            BrowserFactory.CloseAllDrivers();
-            Env.WriteExtentReport();
+
         }
 
-        public static void RecordTestResult()
+        [AfterTestRun]
+        private static void AfterAll()
         {
-            
-            if (ScenarioContext.Current.TestError != null)
-            {
-                var error = ScenarioContext.Current.TestError;
-                Env.test.Log(LogStatus.Fail, error.GetBaseException());
-            }
-            else
-            {
-                string message = "Scenario: " + ScenarioContext.Current.ScenarioInfo.Title + " PASSED";
-                Env.test.Log(LogStatus.Pass, message);
-            }
+            Env.GetExtentReport().Flush();
+            Env.ClearConfig();
+            BrowserFactory.CloseAllDrivers();
         }
     }
 }
