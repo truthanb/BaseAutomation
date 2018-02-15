cac09edc2236f262fb0113aa9f14a84abdadc1e4
Benjamin Truthan <ben.truthan.w@gmail.com>
2018-02-15 08:49:32 -0500

    Added basic util methods to scroll and wait

--- AutomationFramework/framework/Utils.cs
+++ AutomationFramework/framework/Utils.cs
@@ -16,5 +16,26 @@ namespace BaseAutomation.framework
             ((IJavaScriptExecutor)driver).ExecuteScript(@"$(arguments[0]).css({ ""border - width"" : ""2px"", ""border - style"" : ""solid"", ""border - color"" : ""red"" }); ",
                 element);
         }
+        
+        public static void ScrollToElement(IWebElement element)
+        {
+            IWebDriver driver = Env.GetBrowser();
+            var jsDriver = (IJavaScriptExecutor)driver;
+            jsDriver.ExecuteScript("arguments[0].scrollIntoView(true);", element);
+            Thread.Sleep(500);
+        }
+
+        public static void ScrollByCoordinates(string x, string y)
+        {
+            IWebDriver driver = Env.GetBrowser();
+            var jsDriver = (IJavaScriptExecutor)driver;
+            jsDriver.ExecuteScript("window.scrollBy(arguments[0], arguments[1]);", x, y);
+            Thread.Sleep(500);
+        }
+        
+        public static WebDriverWait Wait(double timeout)
+        {
+            return new WebDriverWait(Env.GetBrowser(), TimeSpan.FromSeconds(timeout));
+        }
     }
 }
