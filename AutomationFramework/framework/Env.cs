068103470fb072b522ad3ee194fe1d37d7ea6bf9
Benjamin Truthan <ben.truthan.w@gmail.com>
2018-02-15 09:03:27 -0500

    Update Env.cs

--- AutomationFramework/framework/Env.cs
+++ AutomationFramework/framework/Env.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using OpenQA.Selenium;
-using BaseAutomation.framework;
 using Newtonsoft.Json;
-using System.IO;
+using OpenQA.Selenium;
 using RelevantCodes.ExtentReports;
+using System;
+using System.IO;
 
 namespace AutomationFramework.framework
 {
@@ -14,22 +13,23 @@ namespace AutomationFramework.framework
     {
         private static IWebDriver browser;
 
-        private static Configuration config;
+        public static Configuration config;
         private static int timeOutDuration = 10;
         private static Boolean configLoaded = false;
-        public static ExtentReports report;
-        public static ExtentTest test;
+        private static ExtentReports extent;
 
-        private Env() { }
+        private Env()
+        {
+        }
 
-        public static String GetBaseUrl()
+        public static string GetBaseUrl()
         {
             return config.base_url;
         }
 
         public static IWebDriver GetBrowser()
         {
-                return browser;
+            return browser;
         }
 
         public static IWebDriver SetBrowser()
@@ -37,7 +37,7 @@ namespace AutomationFramework.framework
             LoadConfig();
             string browserType = config.browser;
             if (config.browser.ToLower() == "no browser")
-            { 
+            {
                 BrowserFactory.CloseAllDrivers();
                 return null;
             }
@@ -61,11 +61,11 @@ namespace AutomationFramework.framework
 
         private static void SetTimeOutDuration(int tod)
         {
-            browser.Manage().Timeouts().ImplicitWait.Equals(TimeSpan.FromSeconds(tod));
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(tod);
         }
 
         /// <summary>
-        /// Loads a config file for relevant test run parameters. 
+        /// Loads a config file for relevant test run parameters.
         /// </summary>
         public static void LoadConfig()
         {
@@ -86,9 +86,9 @@ namespace AutomationFramework.framework
                     configLoaded = true;
                 }
             }
-            catch(IOException e)
+            catch (IOException e)
             {
-                Console.WriteLine("Could not load config file. Using default settings.");
+                Console.WriteLine("Could not load config file. Using default settings. {0}", e);
             }
         }
 
@@ -101,28 +101,16 @@ namespace AutomationFramework.framework
             configLoaded = false;
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
         public static void InitializeExtentReport()
         {
-            report = new ExtentReports(config.report_path, true);
-            report.AddSystemInfo("Browser", config.browser);
+            string dir = config.report_path;
+            var reportPath = Path.Combine(dir, config.browser + "_" + DateTime.Now.ToString("MM_dd_yyyy_hh_mm_ss") + ".html");
+            extent = new ExtentReports(reportPath);
         }
 
-        public static void WriteExtentReport()
+        public static ExtentReports GetExtentReport()
         {
-            report.Flush();
-        }
-
-        public static void BeginExtentTestLogging(string title, string descr)
-        {
-            test = report.StartTest(title, descr);
-        }
-
-        public static void EndExtentTest()
-        {
-            report.EndTest(test);
+            return extent;
         }
     }
 }
